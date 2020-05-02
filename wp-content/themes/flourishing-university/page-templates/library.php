c1c9af1b62ca92d394553aa86e430c844e37620c
Gerard Ketuma <gketuma@gmail.com>
2020-05-02 02:08:26 -0500

    Add Library page.

--- wp-content/themes/flourishing-university/page-templates/library.php
+++ wp-content/themes/flourishing-university/page-templates/library.php
@@ -41,7 +41,7 @@ get_header();
 						</div>
 					</div>
 				</div>
-				<section class='row my-3'>
+				<section class='row equal my-3'>
 					<div class='col-sm-12 col-lg-4'>
 						<div class='curated-card'>
 							<div class='curated-card__top'>
@@ -51,15 +51,15 @@ get_header();
 										<span>Collections</span>
 										<a href='#'>
 											<span>Explore!</span>
-											<?php echo load_inline_svg('blue-arrow-icon.svg'); ?>
+											<?php echo load_inline_svg('icon-blue-arrow-circle.svg'); ?>
 										</a>
 									</span>
 								</div>
-								<div class='c-card__top-body'>
+								<div class='c-card__top-body my-2'>
 									Thematic collections of diverse types of resources such as terms, texts, audio-video, sources, and so forth.
 								</div>
 								<div class='c-card__top-search'>
-									<div class="input-group flex-nowrap header-search mr-2">
+									<div class="input-group flex-nowrap header-search my-2">
 										<input type="search" class="form-control py-2 border-right-0" placeholder="Contemplative Practices..." aria-label="Search" aria-describedby="addon-wrapping">
 										<span class="input-group-append">
 											<button class="btn border-left-0 border search-btn" type="button">
@@ -69,13 +69,13 @@ get_header();
 									</div>
 								</div>
 							</div>
-							<div class='curated-card__btm'>
+							<div class='curated-card__btm p-2 pt-3 mt-3'>
 								<div class='c-card__btm-feat'>Featured Collection</div>
-								<div class='c-card__btm-header'>
-									<span>Contemplative Traditions</span>
+								<div class='c-card__btm-header pb-1'>
+									<span class='mr-2'>Contemplative Traditions</span>
 									<?php echo load_inline_svg('exercises-orange-arrow.svg'); ?>
 								</div>
-								<div class='c-card__btm-body'>
+								<div class='c-card__btm-body my-2'>
 									Presenting contemplative traditions through their individual practices and the integration of those practices into broader pathways of practice.
 								</div>
 								<div class='c-card__btm-img'>
@@ -84,7 +84,91 @@ get_header();
 							</div>
 						</div>
 					</div><!-- .col-sm-12.col-lg-4 -->
-				</section><!-- section.row -->
+					<div class='col-sm-12 col-lg-4'>
+						<div class='curated-card'>
+							<div class='curated-card__top'>
+								<div class='c-card__top-header'>
+									<?php echo load_inline_svg('icon-practices-green-treepose-sized.svg'); ?>
+									<span class='c-card__top-header-title ml-2'>
+										<span>Practices</span>
+										<a href='#'>
+											<span>Explore!</span>
+											<?php echo load_inline_svg('icon-blue-arrow-circle.svg'); ?>
+										</a>
+									</span>
+								</div>
+								<div class='c-card__top-body my-2'>
+									Formal contemplative practices done on a regular basis with dedicated attention; Informal practices integrated with other activities.
+								</div>
+								<div class='c-card__top-search'>
+									<div class="input-group flex-nowrap header-search my-2">
+										<input type="search" class="form-control py-2 border-right-0" placeholder="Search Practices..." aria-label="Search" aria-describedby="addon-wrapping">
+										<span class="input-group-append">
+											<button class="btn border-left-0 border search-btn" type="button">
+												<i class="fa fa-search text-black-50"></i>
+											</button>
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class='curated-card__btm p-2 pt-3 mt-3'>
+								<div class='c-card__btm-feat'>Featured Practice</div>
+								<div class='c-card__btm-header pb-1'>
+									<span class='mr-2'>A Practice of Fellowship</span>
+									<?php echo load_inline_svg('exercises-orange-arrow.svg'); ?>
+								</div>
+								<div class='c-card__btm-body my-2'>
+									A practice that directly works with an ability to strengthen compassion towards people beyond an “in group” such as family, friends, and people who we perceive to be similar to us.
+								</div>
+								<div class='c-card__btm-img'>
+									<img src='<?php echo get_stylesheet_directory_uri() . '/img/' . 'library-practice-fellowship@2x.jpg'; ?>' alt='A Practice of Fellowship'>
+								</div>
+							</div>
+						</div>
+					</div><!-- .col-sm-12.col-lg-4 -->
+					<div class='col-sm-12 col-lg-4'>
+						<div class='curated-card'>
+							<div class='curated-card__top'>
+								<div class='c-card__top-header'>
+									<?php echo load_inline_svg('icon-library-exercise-green.svg'); ?>
+									<span class='c-card__top-header-title ml-2'>
+										<span>Exercises</span>
+										<a href='#'>
+											<span>Explore!</span>
+											<?php echo load_inline_svg('icon-blue-arrow-circle.svg'); ?>
+										</a>
+									</span>
+								</div>
+								<div class='c-card__top-body my-2'>
+									Informal one-time activities on a given theme or topic that support reflection, analysis, inquiry, and exchange.
+								</div>
+								<div class='c-card__top-search'>
+									<div class="input-group flex-nowrap header-search my-2">
+										<input type="search" class="form-control py-2 border-right-0" placeholder="Search Exercises..." aria-label="Search" aria-describedby="addon-wrapping">
+										<span class="input-group-append">
+											<button class="btn border-left-0 border search-btn" type="button">
+												<i class="fa fa-search text-black-50"></i>
+											</button>
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class='curated-card__btm p-2 pt-3 mt-3'>
+								<div class='c-card__btm-feat'>Featured Exercise</div>
+								<div class='c-card__btm-header pb-1'>
+									<span class='mr-2'>Contemplative Inquiry Exercises</span>
+									<?php echo load_inline_svg('exercises-orange-arrow.svg'); ?>
+								</div>
+								<div class='c-card__btm-body my-2'>
+									Participants observe an object, image, or person three different ways and, each time, write what they “know” about the object.
+								</div>
+								<div class='c-card__btm-img'>
+									<img src='<?php echo get_stylesheet_directory_uri() . '/img/' . 'library-contemplative-inquiry-exercise@2x.jpg'; ?>' alt='Contemplative Inquiry Exercises'>
+								</div>
+							</div>
+						</div>
+					</div><!-- .col-sm-12.col-lg-4 -->
+				</section><!-- section.row.equal.my-3 -->
 			</div>
 			<div class='col-sm-12 col-lg-4 right-sidebar'>Search Bar</div>
 		</div>
