9331ed5429a3f5b3cafb72fcfa6c686408c36a70
Mark Ferrara <mf8yk@virginia.edu>
2020-05-24 20:21:47 -0600

    SS Updates to Bootstrap column classes for page layouts and Google font imports
    SS Updates to Bootstrap column classes for page layouts and Google font
    imports

--- wp-content/themes/flourishing-university/page-templates/two-three-columns.php
+++ wp-content/themes/flourishing-university/page-templates/two-three-columns.php
@@ -16,7 +16,7 @@ get_header();
 <div class='two-three-column-wrapper'>
 	<div class='container-xxl'>
 		<div class='row'>
-			<div class='col-sm-12 col-lg-8 content pb-5'>
+			<div class='col-sm-12 col-md-8 col-lg-9 content pb-5'>
 				<div class='row'>
 					<div class='col-sm-12'>
 						<div class='title-bar my-2'>
@@ -344,7 +344,7 @@ get_header();
 					</div>
 				</div> <!-- .row -->
 			</div>
-			<div class='col-sm-12 col-lg-4 right-sidebar'>Search Bar</div>
+			<div class='col-sm-12 col-md-4 col-lg-3 right-sidebar'>Search Bar</div>
 		</div>
 	</div>
 </div><!-- two-three-column-wrapper -->
--- wp-content/themes/flourishing-university/page-templates/two-three-rows.php
+++ wp-content/themes/flourishing-university/page-templates/two-three-rows.php
@@ -16,7 +16,7 @@ get_header();
 <div class='two-three-rows-wrapper'>
 	<div class='container-xxl'>
 		<div class='row'>
-			<div class='col-sm-12 col-lg-8 content pb-5'>
+			<div class='col-sm-12 col-md-8 col-lg-9 content pb-5'>
 				<div class='row'>
 					<div class='col-sm-12 mb-3'>
 						<div class='title-bar my-2'>
@@ -376,7 +376,7 @@ get_header();
 					</div><!-- .col-sm-12 -->
 				</section><!-- section.row -->
 			</div>
-			<div class='col-sm-12 col-lg-4 right-sidebar'>Search Bar</div>
+			<div class='col-sm-12 col-md-4 col-lg-3 right-sidebar'>Search Bar</div>
 		</div>
 	</div>
 </div><!-- two-three-column-wrapper -->
